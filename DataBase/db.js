140832aa0c3ac6f57133d645d3d375ec34d09fb8
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-08-23 21:02:07 +0530

    minor changes

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,15 +1,1 @@
-const mongoose = require('mongoose');
-const mongoURL = "mongodb+srv://razeenrasheed83:bjyhvg2ojt0YsalY@cluster0.ntrmq.mongodb.net/"
-
-const connectionOptions = {
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  };
-
-  
-//connection should be aviailable for all components
-const connection = ()=>{
-    return mongoose.createConnection(mongoURL,connectionOptions)
-}
-
-module.exports= {connection}
+const first = useContext(second)
--- controller/controller.js
+++ controller/controller.js
@@ -69,6 +69,7 @@ const createUserData = async (request,response) =>{
     try{
         const data = fs.readFileSync('./users/users.json')
         const JSONdata = JSON.parse(data.toString())
+        console.log(JSONdata)
         JSONdata.push({
             first_name,
             last_name,
